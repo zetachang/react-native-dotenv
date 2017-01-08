06d5aad8f3ed5115e2b9383d4f8e60bc18fd7879
David Chang <zeta11235813@gmail.com>
2017-01-09 04:21:06 +0800

    Use RN’s projectRoots resolver to set the correct configDir

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var config = require("react-native/packager/rn-cli.config");
+
 module.exports = {
   plugins: [
     [require('babel-plugin-dotenv'), {
-      replacedModuleName: 'react-native-dotenv'
+      replacedModuleName: 'react-native-dotenv',
+      configDir: config.getProjectRoots()[0]
     }],
   ],
 };
